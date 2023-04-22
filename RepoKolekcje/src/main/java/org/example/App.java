6c2811640e4f8a14ed85afc557be2e0cff3f6b2a
KaSuchanek <ksuchanek@gmail.com>
2023-04-22 14:27:14 +0200

    new line

--- RepoKolekcje/src/main/java/org/example/App.java
+++ RepoKolekcje/src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello " );
 
     // 2.Stwórz dwa HashSety integerów i zaimplementuj dla nich następujące działania rachunków zbiorów: suma, część wspólna,
     // różnica, suma bez części wspólnej. Wynik działania powinien trafić do trzeciego zbioru.
