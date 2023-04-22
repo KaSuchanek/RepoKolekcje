e32d7bafc0adb569c0e642b61084712f6e19243f
KaSuchanek <ksuchanek@gmail.com>
2023-04-22 14:19:14 +0200

    czesc wspolna

--- RepoKolekcje/src/main/java/org/example/App.java
+++ RepoKolekcje/src/main/java/org/example/App.java
@@ -28,27 +28,24 @@ public class App
         second.add(8);
         second.add(9);
 
-        Integer sum = 0;
-
+        for (Integer i : names) {
+            for (Integer j : second) {
+                if (i == j)
+                    System.out.println("Czesc wspolna " + j);
+            }
+        }
 
 
         // Stwórz dwie HashMapy<String, Integer> (jak w liście zakupów) i zaimplementuj ich sumowanie. Np jedna mapa to Jajka:
         // 3; Mleko: 1, a druga to Jajka: 2; Kiełbasa: 2, wynik to Jajka: 5; Mleko: 1; Kiełbasa: 2
 
-        Map<String, Integer> groceries = new HashMap<>();
-        groceries.put("Eggs", 3);
-        groceries.put("Milk", 1);
-
-        Map<String, Integer> shopping = new HashMap<>();
-        shopping.put("Eggs", 2);
-        shopping.put("Sausage", 2);
-
-
-
-
-
-
-
+//        Map<String, Integer> groceries = new HashMap<>();
+//        groceries.put("Eggs", 3);
+//        groceries.put("Milk", 1);
+//
+//        Map<String, Integer> shopping = new HashMap<>();
+//        shopping.put("Eggs", 2);
+//        shopping.put("Sausage", 2);
 
 
     }
